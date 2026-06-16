1ef34448fea1befa9954675392d33ca4d95c4d05
Darius <darius@thememedb.com>
2026-06-16 16:33:06 +0300

    Landing page (#6)

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<header>
+	<a href="/">
+		<h4 class="logo">memedb</h4>
+	</a>
+	<a href="/app">
+		<img src="/icons/cloudArrowUp.svg" alt="Icon" width="24" height="24" />
+	</a>
+	<a href="/app">
+		<img src="/icons/handCoinsRegular.svg" alt="Icon" width="24" height="24" />
+	</a>
+	<a href="/app">
+		<img src="/icons/funnel.svg" alt="Filter" width="24" height="24" />
+	</a>
+	<a href="/app">
+		<img src="/icons/menuBurger.svg" alt="IcoMenu" width="32" height="32" />
+	</a>
+</header>
+<main>
+	{@render children()}
+</main>
+
+<style>
+	header {
+		height: 64px;
+		background-color: rgba(0, 0, 0, 0.85);
+		position: fixed;
+		top: 0px;
+		left: 50%;
+		transform: translate(-50%, 0%);
+		margin: 0 auto;
+		width: 376px;
+		max-width: 376px;
+		display: flex;
+		justify-content: end;
+		gap: 24px;
+		align-items: center;
+		padding: 16px;
+	}
+	.logo {
+		--gradient: linear-gradient(
+			-150deg,
+			#ff00ff 0%,
+			#00d4ff 40%,
+			#00ff99 50%,
+			#ffff00 70%,
+			#ff7700 85%,
+			#ff33bb 100%
+		);
+		margin: 0;
+		font-size: 36px;
+		font-weight: 700;
+		letter-spacing: -0.03em;
+		line-height: 1;
+		display: block;
+
+		background: var(--gradient);
+		-webkit-background-clip: text;
+		background-clip: text;
+		color: transparent;
+	}
+	main {
+		padding-top: 64px;
+		width: 376px;
+		max-width: 376px;
+		height: 100dvh;
+		margin: 0 auto;
+		background-color: black;
+	}
+	a {
+		color: var(--link);
+		text-decoration: none;
+	}
+	a:hover {
+		color: var(--link-hover);
+	}
+	a:active {
+		color: var(--link-active);
+	}
+</style>
---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import thumbs from '$lib/assets/thumbs.json';
+	const items = thumbs.items ?? [];
+</script>
+
+<main>
+	{#each items as item}
+		<div
+			class="square"
+			style="background-image: url({'https://thumb.pr0gramm.com/' +
+				item.thumb}); background-size: cover; background-position: center;"
+		>
+			<!-- <img src={'https://thumb.pr0gramm.com/' + item.thumb} alt={'thumb-' + item.id} loading="lazy" /> -->
+		</div>
+	{/each}
+</main>
+
+<style>
+	h1 {
+		color: white;
+	}
+	main {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
+		gap: 4px;
+		background-color: transparent;
+	}
+	.square {
+		--size: 120px;
+		width: var(--size);
+		height: var(--size);
+		background-color: var(--gray40);
+		border-radius: 4px;
+	}
+	.square img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		display: block;
+	}
+
+	@media (width < 376px) {
+		main {
+			width: 100dvw;
+			/* height: 100dvh; */
+		}
+		.square {
+			--size: calc((100dvw - 16px) / 3);
+			width: var(--size);
+			height: var(--size);
+		}
+		img {
+			width: 100%;
+			height: 100%;
+			object-fit: scale-down;
+			display: block;
+		}
+	}
+</style>
